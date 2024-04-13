08991147c9bc61c118de7f28b285b1be3053e94c
Raj Aryan <rajxryn@gmail.com>
2024-04-13 17:06:37 +0530

    readjustment
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import { ModeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-mono bg-gradient-to-r from-blue-700/10 to-red-600/10">
-      <div>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-64 p-24 font-mono bg-gradient-to-r from-blue-700/10 to-red-600/10">
+      <div className="flex flex-col items-center">
         <h1 className="font-bold text-7xl">Hello Next.js Template!</h1>
-        <h2 className="text-5xl">
-          Includes - Tailwind CSS, Shadcn, Typescript, Inter Font, Next-themes
+        <h2 className="text-3xl mt-6">
+          Includes - Tailwind CSS, Shadcn, Typescript, Next-themes, Theme Toggle 
         </h2>
       </div>
       <ModeToggle></ModeToggle>
